c6d03731f68d0e7f279a786b8fd9df8d4d699561
kanduri.deepika <Kanduri.Deepika@philips.com>
2019-07-17 11:49:39 +0530

    added files

---
+++ AutoBuildTest.bat
@@ -0,0 +1,3 @@
+echo Build Started !!
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe C:\Users\320052452\BootCamp_CaseStudy_one\BootCamp_CaseStudy_one\BootCamp_CaseStudy_one.csproj
+C:\Users\320052452\BootCamp_CaseStudy_one\BootCamp_CaseStudy_one\bin\Debug\BootCamp_CaseStudy_one.exe
